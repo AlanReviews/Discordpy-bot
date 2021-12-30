a4fb5df0eb346500c043583c7aac6affcf38c3c6
Alan Zhou <54216387+AlanReviews@users.noreply.github.com>
2021-12-30 11:08:54 -0500

    Added Ubuntu and Stats command
    I added the Ubuntu png file for the Ubuntu command. I added the stats command.

--- cogs/Basics.py
+++ cogs/Basics.py
@@ -1,4 +1,5 @@
 import discord, os, random, requests, json
+from discord import File, Embed, channel
 from discord.ext import commands
 
 def get_quote():
@@ -16,27 +17,46 @@ class Basics(commands.Cog):
     # Here's a list of basic commands I created
     @commands.command()
     async def ping(self, ctx, name: str = None):
+        """Check my latency"""
         await ctx.send(f'My ping is {self.bot.latency * 100} s!')
 
     @commands.command()
     async def hug(self, ctx, name: str = None):
+        """I give users a hug"""
         name = name or ctx.author.name
         await ctx.send(f"I give a hug to {name}!")
 
     @commands.command()
     async def eightball(self, ctx):
+        """I predict the future"""
         output = ["Yes", "No", "Unsure", "Can't answer right now"]
         await ctx.send(output[random.randint(0, len(output))])
 
     @commands.command()
     async def echo(self, ctx, *args):
+        """I repeat what you say"""
         await ctx.send(' '.join(ctx.message.content.split()[1:]))
         await ctx.message.delete()
 
     @commands.command()
     async def advice(self, ctx):
+        """I give advice from Zen Quotes"""
         advice = get_quote()
         await ctx.send(advice)
+
+    @commands.command()
+    async def stats(self, ctx):
+        """Check out my stats"""
+        embed = Embed(title="Stats", description="Here's my stats:")
+        embed.add_field(name="Operating System", value="Ubuntu 20.04", inline=False)
+        embed.add_field(name="Status", value="online and working", inline=False)
+        await ctx.send(embed=embed)
+
+    @commands.command()
+    async def ubuntu(self, ctx):
+        """Shows Ubuntu logo in ASCII format"""
+        await ctx.send(file=File(fp="cogs/Ubuntu.png"))
+
 
 def setup(bot):
     bot.add_cog(Basics(bot))
