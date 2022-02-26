628578267568d1638335033cf9cec9d82f9db0b5
Alan Zhou <54216387+AlanReviews@users.noreply.github.com>
2022-02-25 22:00:09 -0500

    Updated quote command
    Replaced request with aiohttp

--- cogs/Basics.py
+++ cogs/Basics.py
@@ -1,14 +1,6 @@
-import discord, os, random, requests, json
+import discord, os, random, aiohttp, json
 from discord import File, Embed, channel
 from discord.ext import commands
-
-def get_quote():
-    # Call a get request
-    response = requests.get("https://zenquotes.io/api/random")
-    # Extract the JSON Data
-    json_data = json.loads(response.text)
-    quote = json_data[0]['q'] + " -" + json_data[0]['a']
-    return (quote)
 
 class Basics(commands.Cog):
     def __init__(self, bot):
@@ -41,9 +33,14 @@ class Basics(commands.Cog):
     @commands.command()
     async def quote(self, ctx):
         """I give a quote from Zen Quotes"""
-        zenquote = get_quote()
-        await ctx.send(zenquote)
-
+        # zenquote = get_quote()
+        # await ctx.send(zenquote)
+        async with aiohttp.ClientSession() as cs:
+            async with cs.get("https://zenquotes.io/api/random") as r:
+                res = await r.json()  # returns dict
+                quote = res[0]['q'] + " -" + res[0]['a']
+                await ctx.send(quote)
+    
     @commands.command()
     async def stats(self, ctx):
         """Check out my stats"""
@@ -68,6 +65,11 @@ class Basics(commands.Cog):
 
         result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
         await ctx.send(result)
+        
+    @commands.command(name='dm')
+    async def dm(self, ctx):
+        """Sends a hello in dms"""
+        await ctx.message.author.send("Hello there")
 
 
 def setup(bot):
