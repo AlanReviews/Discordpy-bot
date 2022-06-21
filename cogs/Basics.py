d88c2bd75d43c75762a568a58067d521c97367f5
Alan Zhou <alanzhou@cmail.carleton.ca>
2022-06-21 17:54:17 -0400

    Added basics.py and main.py files to commit

--- cogs/Basics.py
+++ cogs/Basics.py
@@ -81,7 +81,7 @@ class Basics(commands.Cog):
     @commands.command(name='faq')
     async def faq(self, ctx):
         """Sends you a link to Alan Reviews FAQ page"""
-        await ctx.message.send("What do I review? How do I request a video review? All the answers are in this link: https://principled-lychee-346.notion.site/12bf6d65f1d74780937fa8ae3ed70fe4?v=fd59a2938e614e988fdcb69914306633")
+        await ctx.send("What do I review? How do I request a video review? All the answers are in this link: https://principled-lychee-346.notion.site/12bf6d65f1d74780937fa8ae3ed70fe4?v=fd59a2938e614e988fdcb69914306633")
 
 
 def setup(bot):
---
+++ main.py
@@ -0,0 +1,33 @@
+import discord, os, asyncio
+from discord.ext import commands
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DESCRIPTION = '''Hello, I am Tara. I am a custom discord.py bot created by Alan.'''
+
+intents = discord.Intents.default()
+intents.members = True
+
+TOKEN = os.getenv("DISCORD_TOKEN")
+PREFIX = os.getenv("PREFIX", default="!!")
+activity = discord.Game(name="!!help")
+
+client = commands.Bot(command_prefix=commands.when_mentioned_or(PREFIX), description=DESCRIPTION, intents=intents, activity=activity, status=discord.Status.online)
+
+@client.event
+async def on_ready():
+    print(f"Logged in as {client.user} (ID: {client.user.id})")
+
+initial_extensions = []
+
+
+for filename in os.listdir('./cogs'):
+    if filename.endswith('.py'):
+        initial_extensions.append("cogs." + filename[:-3])
+
+if __name__ == "__main__":
+    for extension in initial_extensions:
+        client.load_extension(extension)
+
+client.run(TOKEN)
