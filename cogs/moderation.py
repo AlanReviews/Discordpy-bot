c7ca18f566c6e69ff50e873c51d999d157f4c187
Alan Zhou <alanzhou@cmail.carleton.ca>
2022-12-02 21:40:51 -0500

    Added a ban command

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -1,10 +1,9 @@
-import discord, os, random, requests, json
+import discord
 from discord import channel
 from discord.ext import commands
 from discord import Embed, member
 from discord.ext.commands import has_permissions, MissingPermissions
 from typing import Optional
-
 class moderation(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
@@ -38,9 +37,18 @@ class moderation(commands.Cog):
     async def kick(self, ctx, member: discord.Member, *, reason):
         """Removes a member from the server. The member can rejoin that server. Only members with the kick members permission can use this command."""
         await member.kick(reason=reason)
-        await ctx.send(f'User {member} has been kicked')
-        await ctx.message.author.send(f'You got kicked for {reason}. Come back when you behave.')
+        await ctx.send(f'User {member} has been kicked!')
 
+
+    @commands.command()
+    @commands.has_permissions(ban_members=True)
+    async def ban(self, ctx, member: discord.Member, *, reason = "not specified"):
+        """Removes a member from a server. The member cannot rejoin the server until the member gets unbanned. Only members with the ban members permission can use this command."""
+        try:
+            await member.ban(delete_message_days=7, reason=reason)
+        except Exception as e:
+            return await ctx.send(e)
+        await ctx.send(f'User {member} got banned!')
 
 async def setup(bot):
     await bot.add_cog(moderation(bot))
